098ab8f4b1ae8a84bb217e60b9ddafc35097f3a7
manoj <manojsaharia45@gmail.com>
2020-08-15 08:20:38 +0530

    initialize project

---
+++ app/src/main/java/com/app/trueleap/messaging/MessagingFragment.java
@@ -0,0 +1,16 @@
+package com.app.trueleap.messaging;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.app.trueleap.R;
+
+public class MessagingFragment extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_messaging_fragment);
+    }
+}
